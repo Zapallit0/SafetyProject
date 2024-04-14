2375e9be94855e08afdfdafc67f4b7995f5c312d
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2024-04-14 16:28:38 -0500

    Home Page almost complete

--- src/Components/Home/Contact/Contact.jsx
+++ src/Components/Home/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import './Contact.css'
 import Facebook from '../../../assets/social/facebook.svg';
 import Linkedin from '../../../assets/social/linkedin.svg';
 import Instagram from '../../../assets/social/instagram.svg';
+
 function Contact() {
   return (
     <div className='Contacto'>
@@ -19,8 +20,8 @@ function Contact() {
           </div>
           <div className='logosRedes'>
             <img src={Facebook} alt="Facebook" />
-            <img src={Instagram} alt="Instagram" />
-            <img src={Linkedin} alt="Linkedin" />
+            <img src={Linkedin} alt="Instagram" />
+            <img src={Instagram} alt="Linkedin" />
           </div>
         </div>
         <div className='FormContacto'>
@@ -40,7 +41,7 @@ function Contact() {
                     </div>
                     <h5>Mensaje:</h5>
                     <textarea className='msg' name='message' rows="5" placeholder='Your Message' ></textarea>
-                    <input type='button' value='Enviar' className='submitBtn' />
+                    <input type='submit' value='Enviar' className='submitBtn' />
                 </form>
                 <div className='MapaDesktop'>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8758.218003714106!2d-77.04476658013421!3d-12.08868059939555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c8584260ca6b%3A0x959f6c5a7bc92394!2sCondominio%20Ambassador%2C%20cuadra%2025%20del%20Jr.%20Brigadier%20Mateo%20Pumacahua%2C%20Urb.%20Risso%2C%20Per%C3%BA.%2C%20Jr.%20Brigadier%20Pumacahua%202550%2C%20Lince%2015073%2C%20Per%C3%BA!5e0!3m2!1ses-419!2smx!4v1707480443909!5m2!1ses-419!2smx"
