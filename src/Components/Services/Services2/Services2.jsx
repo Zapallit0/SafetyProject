97e80fe1e438b525defad6bf0b951457ec0d759a
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2024-04-23 23:08:44 -0500

    Cambios Servicios, sin completar 4horas

--- src/Components/Services/Services2/Services2.jsx
+++ src/Components/Services/Services2/Services2.jsx
@@ -1,49 +1,67 @@
 import React from 'react'
-import EnvironmentBackground from '../../../assets/ServicesPage/BackgroundSaludOcupacional.jpg'
-import VigMedica from "../../../assets/ServicesPage/Gestion de Salud Ocupacional/Servicio1.webp"
-import AgenOcupacional from "../../../assets/ServicesPage/Gestion de Salud Ocupacional/Servicio2.jpg"
-import HigOcupacional from "../../../assets/ServicesPage/Gestion de Salud Ocupacional/Servicio3.jpg"
-import DwnldButton from "../../../assets/ServicesPage/Global/DownloadArrow.svg"
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import CheckList from "../../../assets/IconsList/checklist.svg"
+import './Services2.css'
+import DwnldButton from "../../../assets/IconsList/DownloadArrow.svg"
 function Services2() {
   const ServicesEnvironment =[{
     "ServiceName": "Programas de vigilancia médica ocupacional",
-    "Image": VigMedica
   }, {
     "ServiceName": "Monitoreos de Agentes Ocupacionales",
-    "Image": AgenOcupacional
   },{
     "ServiceName": "Programas de Higiene Ocupacional",
-    "Image": HigOcupacional
   }
 ];
   return (
-    <div className='Services1'>
-      <div className='Services1_Title'>
-        <img src={EnvironmentBackground} alt="Security Back" />
-        <div className='Services1_Title_Content'>
+    <div className='TwoServices'>
+      <div className='Services2'>
+      <div className='Services2_Title'>
           <h3>
             Gestión de Salud Ocupacional
           </h3>
           <span>
             según Ley 29783, DS 005-2012 TR, RM 375-2088 TR
           </span>
-        </div>
     
       </div>
-      <button className='downloadBtn'>
-        <a>
-          <img src={DwnldButton} alt="DescargaBrouche" /> Descargar Brouche
-        </a>
-      </button>
-      <div className='SecurityServices'>
+      <div className='ListServices'>
         {
           ServicesEnvironment.map((service,index)=>(
             <div className='CardService'>
-              <img src={service.Image} alt="Servicio"/>
+              <LazyLoadImage src={CheckList}/>
               <h4>{service.ServiceName}</h4>
             </div>
-          ))
-        }
+          ))}
+          <button className='downloadBtn'>
+            <a>
+              <LazyLoadImage src={DwnldButton} alt="DescargaBrouche" /> Descargar Brouche
+            </a>
+          </button>
+      </div>
+      </div>
+      <div className='Services2'>
+        <div className='Services2_Title'>
+            <h3>
+            Gestión de Prevención de Riesgos
+            </h3>
+            <span>
+              según Ley 29783, DS 005-2012 TR, RM 375-2088 TR
+            </span>
+        </div>
+        <div className='ListServices'>
+        {
+          ServicesEnvironment.map((service,index)=>(
+            <div className='CardService'>
+              <LazyLoadImage src={CheckList}/>
+              <h4>{service.ServiceName}</h4>
+            </div>
+          ))}
+          <button className='downloadBtn'>
+            <a>
+              <LazyLoadImage src={DwnldButton} alt="DescargaBrouche" /> Descargar Brouche
+            </a>
+          </button>
+      </div>
       </div>
     </div>
   )
