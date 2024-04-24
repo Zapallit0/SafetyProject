5d14af5e640098823c7cde3ee5ff9980972ec56c
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2024-04-23 22:48:50 -0500

    Changes in Services new desing

--- src/Components/Services/Services1/Services1.jsx
+++ src/Components/Services/Services1/Services1.jsx
@@ -1,63 +1,42 @@
 import React from 'react'
 import './Services1.css'
-import SecurityBackground from '../../../assets/ServicesPage/BackgroundGestionSeg.jpg'
-import SSTimg from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service1.png"
-import IPERCimg from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service2.jpg"
-import ReglaIntern from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service3.png"
-import PlanSeg from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service4.jpg"
-import PlanCap from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service5.webp"
-import PlanEmer from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service6.jpg"
-import InvesAccidentes  from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service7.png"
-import ProcedEstan from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service8.jpg"
-import EstadisticaSeg from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service9.jpg"
-import PlanAudit from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service10.jpg"
-import MatrizLegal from "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service11.jpg"
-import ComitePar from  "../../../assets/ServicesPage/Sistema de Gestión Seguridad y Salud en el Trabajo/Service12.jpg"
+import CheckList from "../../../assets/IconsList/checklist.svg"
 import DwnldButton from "../../../assets/ServicesPage/Global/DownloadArrow.svg"
 import { Button } from 'react-scroll'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 function Services1() {
-  const ServicesSecurity =[
-  {"ServiceName":"Política de SST(Seguridad Salud y Trabajo)", "Img":SSTimg},
-  {"ServiceName":"IPERC (Identificación de Peligros y la Evaluación de Riesgos y Controles )", "Img":IPERCimg},
-  {"ServiceName":"Reglamento Interno de Seguridad y Salud en el Trabajo", "Img":ReglaIntern},
-  {"ServiceName":"Plan / Programa Anual de Seguridad y Salud Ocupacional", "Img":PlanSeg},
-  {"ServiceName":"Plan / Programa anual de capacitación e inspecciones.", "Img":PlanCap},
-  {"ServiceName":"Plan de Respuesta a Emergencias, formación de Brigada de Emergencia.", "Img":PlanEmer},
-  {"ServiceName":"Investigación de Accidentes/Enfermedades", "Img":InvesAccidentes},
-  {"ServiceName":"Procedimientos y estándares", "Img":ProcedEstan},
-  {"ServiceName":"Estadística e indicadores de Seguridad", "Img":EstadisticaSeg},
-  {"ServiceName":"Plan de Auditorías internas y externas", "Img":PlanAudit},
-  {"ServiceName":"Matriz de Requisitos Legales", "Img":MatrizLegal},
-  {"ServiceName":"Asesoría en conformación y gestión de Comité Paritario.", "Img":ComitePar}
-];
+  const ServicesList=[
+  {"ServiceName":"Política de SST(Seguridad Salud y Trabajo)"},
+  {"ServiceName":"IPERC (Identificación de Peligros y la Evaluación de Riesgos y Controles )"},
+  {"ServiceName":"Reglamento Interno de Seguridad y Salud en el Trabajo"},
+  {"ServiceName":"Plan / Programa Anual de Seguridad y Salud Ocupacional"},
+  {"ServiceName":"Plan / Programa anual de capacitación e inspecciones."},
+  {"ServiceName":"Plan de Respuesta a Emergencias, formación de Brigada de Emergencia."},
+  {"ServiceName":"Investigación de Accidentes/Enfermedades"},
+  {"ServiceName":"Procedimientos y estándares"},
+  ]
   return (
-    <div className='Services1'>
-      <div className='Services1_Title'>
-        <img src={SecurityBackground} alt="Security Back" />
-        <div className='Services1_Title_Content'>
+    <div className='Services'>
+        <div className='Services_Title'>
           <h3>
             Sistema de Gestión Seguridad y Salud en el Trabajo
           </h3>
           <span>según Ley 29783 y reglamiento sectorial</span>
         </div>
-      </div>
-
-      <button className='downloadBtn'>
-        <a>
-          <img src={DwnldButton} alt="DescargaBrouche" /> Descargar Brouche
-        </a>
-      </button>
-    
-      <div className='SecurityServices'>
-        {
-          ServicesSecurity.map((service,index)=>(
-            <div className='CardService'>
-              <img src={service.Img} alt="Servicio" key={index}/>
-              <h4>{service.ServiceName}</h4>
-            </div>
-          ))
-        }
+      <div className='ListServices'>
+      {
+            ServicesList.map((service,index)=>(
+           <div className='CardService' key={index}>
+            <LazyLoadImage src={CheckList}/>
+            <h4>{service.ServiceName}</h4>
+          </div>
+        ))}
+        <button className='downloadBtn'>
+          <a>
+            <LazyLoadImage src={DwnldButton} alt="DescargaBrouche" />BROUCHE COMPLETO
+          </a>
+        </button>
       </div>
     </div>
   )
--- src/Components/Services/Services3/Services3.jsx
+++ src/Components/Services/Services3/Services3.jsx
@@ -1,57 +1,50 @@
 import React from 'react'
-import CapacitacionBackground from '../../../assets/ServicesPage/BackgroundCapacitacion.webp'
 import DwnldButton from "../../../assets/ServicesPage/Global/DownloadArrow.svg"
-import Services1 from "../../../assets/ServicesPage/Salud Ocupacional/Servicio1.webp"
+import CheckList from "../../../assets/IconsList/checklist.svg"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 function Services3() {
   const ServicesSalud =[{
       "ServiceName": "Evaluación de Riesgos Psicosociales",
-      "Image":""
   },
   {
     "ServiceName": "Programas de Seguridad Basada en el Comportamiento (SBC)",
-    "Image":""
 },
 {
   "ServiceName": "Programas Preventivos",
-  "Image":""
 },{
   "ServiceName": "Evaluación de Riesgos Psicosociales",
-  "Image":""
 },{
   "ServiceName": "Elaboración de Perfil de Accidentabilidad.",
-  "Image":""
 },{
   "ServiceName":"Gestión de Riesgos Críticos",
-  "Image":""
 }];
   return (
-    <div className='Services1'>
-      <div className='Services1_Title'>
-        <img src={CapacitacionBackground} alt="Security Back" />
-        <div className='Services1_Title_Content'>
+    <div className='Services'>
+      <div className='Services_Title'>
           <h3>
           Gestión de Prevención de Riesgos
           </h3>
           <span>
             según Ley 29783, DS 005-2012 TR, RM 375-2088 TR
           </span>
-        </div>
-        
       </div>
-      <button className='downloadBtn'>
-        <a>
-          <img src={DwnldButton} alt="DescargaBrouche" /> Descargar Brouche
-        </a>
-      </button>
-      <div className='SecurityServices'>
-        {
-          ServicesSalud.map((service,index)=>(
-            <div className='CardService'>
-              <img src={Services1} alt="" />
-              <h4>{service.ServiceName}</h4>
-            </div>
-          ))
-        }
+      <div className='ListServices'>
+        <div className='SecurityServices'>
+          {
+            ServicesSalud.map((service,index)=>(
+              <div className='CardService'>
+                <LazyLoadImage src={CheckList}/>
+                <h4>{service.ServiceName}</h4>
+              </div>
+            ))
+          }
+        </div>
+        <button className='downloadBtn'>
+          <a>
+            <LazyLoadImage src={DwnldButton} alt="DescargaBrouche" />BROUCHE COMPLETO
+          </a>
+        </button>
       </div>
     </div>
   )
