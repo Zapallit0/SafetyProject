b98b3d078bc141e9a8655ec972dc92ac49e174ec
Jhersvin Villodas Quinto <jhersvinvillodasquinto@gmail.com>
2024-04-29 13:30:57 -0500

    Animacion main menu

--- src/Components/Home/MainPage/MainPage.jsx
+++ src/Components/Home/MainPage/MainPage.jsx
@@ -12,21 +12,27 @@ import Instagram from '../../../assets/social/instagram.svg';
 function MainPage() {
   return (
     <div className='mainPage' id='Main_Page'>
-      <div className='backgroundContainer'>
+      {/* <div className='backgroundContainer'>
         <LazyLoadImage loading="lazy" src={background} alt="Fondo" />
-      </div>
-      <div className='textMP'>
+      </div> */}
         <h1>SAFETY <br />ADVISORS</h1>
-        <h2>Especialista en Seguridad, <br />
-        Salud Ocupacional <br />
-        y Medio Ambiente</h2>
-        <Link to={"/Contacto"}><button type='button'>Contáctenos</button></Link>
-      </div>
-      <div className='redesMP'>
+        <div className='wrapper'>
+          <p>Especialista en </p>
+          <div className='words'>
+            <span>Seguridad</span>
+            <span>Salud Ocupacional</span>
+            <span>Capacitaciones</span>
+            <span>Homologaciones</span>
+            <span>Seguridad</span>
+          </div>
+
+        </div>
+        <Link to={"/Contacto"} className='buttonMain'><button type='button'>Contáctenos</button></Link>
+      {/* <div className='redesMP'>
         <a href="https://www.facebook.com/profile.php?id=61557920314806&mibextid=ZbWKwL" target='_blank'><LazyLoadImage className='iconRed' src={Facebook} alt="Facebook" type="image/svg+xml"/></a>
         <a href="https://www.linkedin.com/company/safety-advisors24/about/" target='_blank'><LazyLoadImage className='iconRed' src={Linkedin} alt="Linkedin" type="image/svg+xml"/></a>
         <a href="https://www.instagram.com/safety__advisors/?igsh=dGNvN3ZiMGtsMDlm" target='_blank'><LazyLoadImage className='iconRed' src={Instagram} alt="Instagram" type="image/svg+xml"/></a>
-      </div>
+      </div> */}
     </div>
   )
 }
