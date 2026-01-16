41d4dcb16cd10889ecae5b507fa247945c38b71b
Jhersvin Villodas <jhersvinvillodasquinto@gmail.com>
2026-01-16 05:56:52 -0500

    Cambiando el homepage e iniciando con la vista movil

--- src/Components/Home/MainPage/MainPage.jsx
+++ src/Components/Home/MainPage/MainPage.jsx
@@ -15,27 +15,26 @@ function MainPage() {
       <div className='information'>
         <img src={Logo_Fullname} alt="Logo Safety Advisor" height={"100px"} width={"300px"}/>
         <div className='wrapper'>
-          <h2>Especialistas en</h2>
+          <h2>ESPECIALISTAS EN</h2>
           <div className='words'>
-            <span>Seguridad</span>
-            <span>Salud Ocupacional</span>
-            <span>Capacitaciones</span>
-            <span>Homologaciones</span>
-            <span>Seguridad</span>
+            <span>SEGURIDAD</span>
+            <span>SALUD OCUPACIONAL</span>
+            <span>CAPACITACIONES</span>
+            <span>HOMOLOGACIONES</span>
+            <span>SEGURIDAD</span>
           </div>
         </div>
         <p>Creando entornos de trabajo seguros para tu empresa</p>
-      </div>
-
         <div className='buttonsMainPage'>
           <Link to={"/Servicios"} className='buttonServicesMP'><button type='button'>Servicios</button></Link>
           <Link to={"/Contacto"} className='buttonCTA'> <button>Contáctanos</button></Link>
         </div>
-        <div className='RedesMP'>
-          <a href="https://www.facebook.com/profile.php?id=61557920314806&mibextid=ZbWKwL" target='_blank'><LazyLoadImage src={Facebook} alt="Safety Advisors Facebook" title='FacebookSA'/></a>
-          <a href="https://www.linkedin.com/company/safety-advisors24/about/" target='_blank'><LazyLoadImage src={Linkedin} alt="Safety Advisors Linkedin" title='LinkedinSA'/></a>
-          <a href="https://www.instagram.com/safety__advisors/?igsh=dGNvN3ZiMGtsMDlm" target='_blank'><LazyLoadImage src={Instagram}  alt="Safety Advisors Instagram" title='InstagramSA'/></a>
-        </div>
+      </div>
+      <div className='RedesMP'>
+        <a href="https://www.facebook.com/profile.php?id=61557920314806&mibextid=ZbWKwL" target='_blank'><LazyLoadImage src={Facebook} alt="Safety Advisors Facebook" title='FacebookSA'/></a>
+        <a href="https://www.linkedin.com/company/safety-advisors24/about/" target='_blank'><LazyLoadImage src={Linkedin} alt="Safety Advisors Linkedin" title='LinkedinSA'/></a>
+        <a href="https://www.instagram.com/safety__advisors/?igsh=dGNvN3ZiMGtsMDlm" target='_blank'><LazyLoadImage src={Instagram}  alt="Safety Advisors Instagram" title='InstagramSA'/></a>
+      </div>
     </div>
   )
 }
