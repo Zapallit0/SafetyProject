086fd085ee8ee8c94005a144405bff0830671b48
Jhersvin Villodas <jhersvinvillodasquinto@gmail.com>
2026-01-20 11:29:29 -0500

    Cambiando nombre de componente de servicios en homepage ademas de cambiar un poco en mobile el navbar y homepage

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,13 +3,13 @@ import MainPage from './Home/MainPage/MainPage.jsx'
 import AboutUs from './Home/AboutUs/AboutUs.jsx'
 import Contact from './Home/Contact/Contact.jsx'
 import Footer from '../SmallComponents/Footer/Footer.jsx'
-import Service2 from './Home/Service2/Service2.jsx'
+import Service_HomePage from './Home/Service_HomePage/Service_HomePage.jsx'
 import transition from '../transition.jsx'
 function Home() {
   return (
     <>
         <MainPage />
-        <Service2/>
+        <Service_HomePage/>
         <AboutUs />
         <Contact />
         <Footer />
