96f49d3b0f5d0627c132073249e2bb329da2fa37
Jhersvin Villodas <jhersvinvillodasquinto@gmail.com>
2026-01-21 12:21:16 -0500

    Cambiando la animacion para usar framer motion

--- src/Components/Home/MainPage/MainPage.jsx
+++ src/Components/Home/MainPage/MainPage.jsx
@@ -1,7 +1,7 @@
 import './MainPage.css'
-// Lazy Load
+import { useState, useEffect} from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-
+import { motion, AnimatePresence } from 'framer-motion'
 import { Link } from 'react-router-dom';
 import Logo_Fullname from '../../../assets/Logos/Logo_FullName_W.svg'
 import Facebook from '../../../assets/social/facebook-SafetyAdvisors.svg';
@@ -10,18 +10,38 @@ import Instagram from '../../../assets/social/instagram-SafetyAdvisors.svg';
 
 
 function MainPage() {
+  const [index, setIndex] = useState(0)
+  const words = [
+    'SEGURIDAD',
+    'SALUD OCUPACIONAL',
+    'CAPACITACIONES',
+    'HOMOLOGACIONES'
+  ]
+  useEffect(() => {
+      const interval = setInterval(() => {
+        setIndex((prev) => (prev + 1) % words.length)
+      }, 2000)
+
+      return () => clearInterval(interval)
+    }, [])
   return (
     <div className='mainPage' id='Main_Page'> 
       <div className='information'>
         <img src={Logo_Fullname} alt="Logo Safety Advisor" height={"100px"} width={"300px"}/>
         <div className='wrapper'>
           <h2>ESPECIALISTAS EN</h2>
-          <div className='words'>
-            <span>SEGURIDAD</span>
-            <span>SALUD OCUPACIONAL</span>
-            <span>CAPACITACIONES</span>
-            <span>HOMOLOGACIONES</span>
-            <span>SEGURIDAD</span>
+           <div className='motion-words'>
+            <AnimatePresence mode="wait">
+              <motion.span
+                key={index}
+                initial={{ y: 30, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                exit={{ y: -30, opacity: 0 }}
+                transition={{ duration: 0.4, ease: 'easeOut' }}
+              >
+                {words[index]}
+              </motion.span>
+            </AnimatePresence>
           </div>
         </div>
         <p>Creando entornos de trabajo seguros para tu empresa</p>
