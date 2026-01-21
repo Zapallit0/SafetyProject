339b2ee92a7f263a684533154932e5a530d62608
Jhersvin Villodas <jhersvinvillodasquinto@gmail.com>
2026-01-21 18:27:21 -0500

    Completado Diseño de HM Responsive

--- src/Components/Home/MainPage/MainPage.jsx
+++ src/Components/Home/MainPage/MainPage.jsx
@@ -46,8 +46,27 @@ function MainPage() {
         </div>
         <p>Creando entornos de trabajo seguros para tu empresa</p>
         <div className='buttonsMainPage'>
-          <Link to={"/Servicios"} className='buttonServicesMP'><button type='button'>Servicios</button></Link>
-          <Link to={"/Contacto"} className='buttonCTA'> <button>Contáctanos</button></Link>
+          <Link to="/Servicios" className="buttonLink">
+            <motion.button
+              className="btn btn-primary"
+              whileHover={{ y: -3, boxShadow: '0px 10px 25px rgba(0,0,0,0.35)' }}
+              whileTap={{ scale: 0.96 }}
+              transition={{ type: 'spring', stiffness: 300, damping: 18 }}
+            >
+              Servicios
+            </motion.button>
+          </Link>
+
+          <Link to="/Contacto" className="buttonLink">
+            <motion.button
+              className="btn btn-outline"
+              whileHover={{ y: -3 }}
+              whileTap={{ scale: 0.96 }}
+              transition={{ type: 'spring', stiffness: 300, damping: 18 }}
+            >
+              Contáctanos
+            </motion.button>
+          </Link>
         </div>
       </div>
       <div className='RedesMP'>
