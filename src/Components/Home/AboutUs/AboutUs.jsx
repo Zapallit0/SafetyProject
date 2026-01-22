35a7437ac25ef2d349db3cb8a8a1dda7137e5035
Jhersvin Villodas <jhersvinvillodasquinto@gmail.com>
2026-01-21 21:11:16 -0500

    Cambios para about us

--- src/Components/Home/AboutUs/AboutUs.jsx
+++ src/Components/Home/AboutUs/AboutUs.jsx
@@ -1,36 +1,60 @@
-import React from 'react'
 import './AboutUs.css'
 import {Link} from 'react-router-dom'
 import Nosotros from'../../../assets/HomePageImgs/AboutUs.webp';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 function AboutUs() {
+    const areas_especializacion=[
+        {   
+            "Index":"1",
+            "Area":"Gestión de Seguridad",
+            "Image":""
+        },
+        {   
+            "Index":"2",
+            "Area":"Salud Ocupacional",
+            "Image":""
+        },
+        {   
+            "Index":"3",
+            "Area":"Medio Ambiente",
+            "Image":""
+        },
+        {   
+            "Index":"4",
+            "Area":"Homologaciones",
+            "Image":""
+        },
+        {   
+            "Index":"5",
+            "Area":"Gestión de Talento y Capacitaciones",
+            "Image":""
+        },
+    ]
   return (
     <div className='AboutUs' id='About_Us'>
-        <LazyLoadImage src={Nosotros} alt="Imagen Equipo Safety Advisors" effect='blur' title='Equipo SA'/>
         <div className='textAboutUs'>
             <div className='textOurInfo'>
-                <h4>Sobre</h4>
-                <h2>NOSOTROS</h2>
-                <p>Somos una empresa comprometida en brindar soluciones 
-                y servicios de calidad dedicada a los servicios de 
-                Asesoria y Consultoria, en las áreas de <span>GESTIÓN DE SEGURIDAD,
-                SALUD OCUPACIONAL, HOMOLOGACIONES,  
-                GESTIÓN DE TALENTO Y CAPACITACIONES</span> 
-                </p>
+                <h4>ÁREAS DE ESPECIALIZACIÓN</h4>
+                <div className='card'>
+                    
+                </div>
             </div>
             <div className='textOurMission'>
-                <h3>Nuestra Misión</h3>
-                <p>SAFETY ADVISORS no solo protegemos, cuidamos y promovemos,
-                    sino que transformamos su organización hacia un futuro más
-                    seguro, saludable y sostenible.
-                </p>
+                <img src="" alt="" />
+                <div className='cardOurMission'>
+                    <h3>No solo te protegemos y cuidamos</h3>
+                    <p>
+                        Tambien transformamos tu organización hacia un futuro más
+                        seguro, saludable y sostenible.
+                    </p>
+                    <Link className='linkAboutUs' to={'/Nosotros'}>
+                        <button>
+                            Conócenos más
+                        </button>
+                    </Link>
+                </div>
             </div>
-            <Link className='linkAboutUs' to={'/Nosotros'}>
-                <button>
-                    Conoce más
-                </button>
-            </Link>
         </div>
     </div>
   )
